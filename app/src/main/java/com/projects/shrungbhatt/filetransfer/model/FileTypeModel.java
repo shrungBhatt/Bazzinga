1d167dc9a0dd0c5af6b6a48faf5849d5356a3862
Shrung Bhatt <huntjigsaw97@gmail.com>
2018-04-16 12:14:09 +0530

    Added the homescreen image

---
+++ app/src/main/java/com/projects/shrungbhatt/filetransfer/model/FileTypeModel.java
@@ -0,0 +1,48 @@
+package com.projects.shrungbhatt.filetransfer.model;
+
+/**
+ * Created by jigsaw on 7/3/18.
+ */
+
+public class FileTypeModel {
+
+    private String mVideo;
+
+    private String mImage;
+
+    private String mFile;
+
+    private String mFileType;
+
+    public String getFileType() {
+        return mFileType;
+    }
+
+    public void setFileType(String fileType) {
+        mFileType = fileType;
+    }
+
+    public String getVideo() {
+        return mVideo;
+    }
+
+    public void setVideo(String video) {
+        mVideo = video;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
+
+    public String getFile() {
+        return mFile;
+    }
+
+    public void setFile(String file) {
+        mFile = file;
+    }
+}
